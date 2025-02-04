03cd3a82e2ebe3a376950fd5e220486fac2dd9ad
aldruin <abl.souza97@gmail.com>
2025-02-04 20:30:10 -0300

    implementa endpoint get pessoas por matricula e associa entidades

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const Controller = require('./Controller.js');
 const PessoaServices = require('../services/PessoaServices.js');
 
@@ -7,6 +8,17 @@ class PessoaController extends Controller {
   constructor() {
     super(pessoaServices);
   }
+
+  async pegaMatriculas(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+      return res.status(200).json(listaMatriculas);
+    } catch (error) {
+      //erro
+    }
+  }
+
 }
 
 //commonJS export
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -15,6 +15,7 @@ router.get('/pessoas/:id', (req, res) => pessoaController.pegaUmPorId(req, res))
 router.post('/pessoas', (req, res) => pessoaController.criaNovo(req, res));
 router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
 router.delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
+router.get('/pessoas/:estudanteId/matriculas', (req, res) => pessoaController.pegaMatriculas(req, res));
 // \/ implementa rota para criação de matriculas, pois de acordo com a regra de negócio, é a partir do estudanteId que se cria, sendo acessado através da rota 'pessoas'
 router.post('/pessoas/:estudanteId/matriculas', (req, res) => matriculaController.criaNovo(req, res));
 
